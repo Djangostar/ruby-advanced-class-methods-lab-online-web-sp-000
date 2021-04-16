3344838b8fc2c9722391da9c9bad82d179ff5eba
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-04-16 05:43:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,20 +17,20 @@ class Song
   end
 
   def self.new_by_name(song_name)
-    song = self.new
+    song = Song.new
     song.name = song_name
     song
   end
 
   def self.create_by_name(song_name)
-    song = self.new
+    song = Song.new
     song.name = song_name
     song.save
     song
   end
 
   def self.find_by_name(song_name)
-    self.all.find {|this_song| this_song.name == song_name}
+    self.all.find {|the_song| the_song.name == song_name}
   end
 
   def self.find_or_create_by_name(song_name)
@@ -38,21 +38,22 @@ class Song
   end
 
   def self.alphabetical
-    self.all.sort_by {|a| a.name}
+    self.all.sort_by {|a| a.name }
   end
 
-  def self.new_from_filename(filename)
+  def self.new_from_filename(file)
     new_song = self.new
-    new_song.name = filename.split(" - ")[1].split(".")[0]
-    new_song.artist_name = filename.split(" - ")[0]
+    new_song.name = file.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file.split(" - ")[0]
     new_song
   end
 
-  def self.create_from_filename(filename)
-      new_song = self.new
-      new_song.name = filename.split(" - ")[1].split(".")[0]
-      new_song.artist_name = filename.split(" - ")[0]
-      new_song.save
+  def self.create_from_filename(file)
+    new_song = self.new
+    new_song.name = file.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file.split(" - ")[0]
+    new_song.save
+    new_song
   end
 
   def self.destroy_all
